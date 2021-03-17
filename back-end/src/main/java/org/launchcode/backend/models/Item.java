a61a37c3f0eec3cd67840e2b63f181a363d923a0
Chan <chandler.eubank@gmail.com>
2021-03-17 18:21:08 -0500

    added persistence

--- back-end/src/main/java/org/launchcode/backend/models/Item.java
+++ back-end/src/main/java/org/launchcode/backend/models/Item.java
@@ -4,3 +4,10 @@ public class Item {
 
 
 }
+
+
+// item repository - anna
+// item controller - tara
+// item class - chandler
+// decide what goes into each class, then filter/create AbstractEntity from there --> look in github at Anya's form for reference
+// repl.it - github
