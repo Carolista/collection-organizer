54d58bb34dd68080e49ac7cdad6413850cc0a379
Anya Wynne <avkwynne@gmail.com>
2021-03-17 09:50:29 -0500

    small change

--- frontEnd/src/app/ItemClass.ts
+++ frontEnd/src/app/ItemClass.ts
@@ -1,7 +1,7 @@
 export class Item {
     public imagePath: string;
     public category: string;
-    public keywords: string;
+    public keywords: string[];
     public title: string;
     public countryCreator: string;
     public year: number;
@@ -10,7 +10,7 @@ export class Item {
     public description: string;
     public references: string;
 
-    constructor(imagePath: string, category: string, keywords: string, title: string, countryCreator: string, 
+    constructor(imagePath: string, category: string, keywords: string[], title: string, countryCreator: string, 
         year: number, condition: string, media: string, description: string, reference: string)
         
         {
