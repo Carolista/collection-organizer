f854e00575831381e53c311254f35731a21cc56c
Anya Wynne <avkwynne@gmail.com>
2021-03-18 10:59:17 -0500

    created a short ItemClass for testing

--- frontEnd/src/app/ItemClass.ts
+++ frontEnd/src/app/ItemClass.ts
@@ -1,28 +1,13 @@
 export class Item {
+    public userID: string;
     public imagePath: string;
-    public category: string;
-    public keywords: string[];
     public title: string;
-    public countryCreator: string;
-    public year: number;
-    public condition: string;
-    public media: string;
-    public description: string;
-    public references: string;
 
-    constructor(imagePath: string, category: string, keywords: string[], title: string, countryCreator: string, 
-        year: number, condition: string, media: string, description: string, reference: string)
+    constructor( userId: string,imagePath: string, title: string )
         
         {
+        this.userID = userId;
         this.imagePath = imagePath;
-        this.category = category;
-        this.keywords = keywords;
-        this.title = title;
-        this.countryCreator = countryCreator;
-        this.year = year;
-        this.condition = condition;
-        this.media = media;
-        this.description = description;
-        this.references = reference; 
+        this.title = title; 
     }
 }
