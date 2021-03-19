5470d998adf5c6ea6bfdf712e08431899582c006
Casey <casey.gardonio@foat.org>
2021-03-19 16:09:52 -0500

    added final routes and connected navbar buttons

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { MemberPageComponent } from './member-page/member-page.component';
 import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MemberPageComponent } from './member-page/member-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberPageComponent,
     NavbarHeaderComponent,
     LandingPageComponent,
+    MemberPageComponent,
     PageNotFoundComponent
   ],
   imports: [
