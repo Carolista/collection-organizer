c3f4872483b8a4adbaf1df1637f5af5cf8cb7116
Casey <casey.gardonio@foat.org>
2021-03-19 16:40:41 -0500

    added url route for add-item-form and embedded on member-page.html template

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { MemberPageComponent } from "./member-page/member-page.component";
+import { AddItemFormComponent } from "./add-item-form/add-item-form.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'member-page', component: MemberPageComponent},
+  {path: 'add-item', component: AddItemFormComponent},
   {path: 'page-not-found', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/page-not-found'}
 ]
