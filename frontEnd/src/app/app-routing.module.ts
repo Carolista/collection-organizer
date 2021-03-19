4d939cdfaf4794934fd16feb70ccc3d8fdf85669
Casey <casey.gardonio@foat.org>
2021-03-19 15:57:44 -0500

    added page-not-found.component; added initial routes to app-routing.module.ts

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { MemberPageComponent } from "./member-page/member-page.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'member-page', component: MemberPageComponent},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/page-not-found'}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+   RouterModule.forRoot(appRoutes, {scrollPositionRestoration: 'enabled'}), 
+   //"scrollPositionRestortion" allows scroll position on newly loaded page to be reset to top of page)
+  ],
+  exports: [RouterModule] 
 })
+
 export class AppRoutingModule { }
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MemberPageComponent } from './member-page/member-page.component';
 import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemberPageComponent,
     NavbarHeaderComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
