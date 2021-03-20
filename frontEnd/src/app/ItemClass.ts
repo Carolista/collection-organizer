e051cbe27dd30255f313d7bf6491481e155bb177
Anya Wynne <avkwynne@gmail.com>
2021-03-20 11:27:53 -0500

    updated short class for testing

--- frontEnd/src/app/ItemClass.ts
+++ frontEnd/src/app/ItemClass.ts
@@ -1,13 +1,11 @@
 export class Item {
     public id: string;
-    public imagePath: string;
     public title: string;
 
-    constructor( id: string,imagePath: string, title: string )
+    constructor( id: string, title: string )
         
         {
         this.id = id;
-        this.imagePath = imagePath;
         this.title = title; 
     }
 }
