badae58c70fac4bfdf7fa6783025d66638d6262c
Anya Wynne <avkwynne@gmail.com>
2021-03-23 15:50:30 -0500

    created basic Item Detail Page

--- frontEnd/src/app/view-list-of-collectibles/item-detail/item-detail.component.ts
+++ frontEnd/src/app/view-list-of-collectibles/item-detail/item-detail.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailComponent implements OnInit {
 
+  itemData = {
+    imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words.  The details of the item will be expended a back-end team will add more controllers and we have one common class to work with.'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
