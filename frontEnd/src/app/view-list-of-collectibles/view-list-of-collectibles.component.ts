88ac8452dc828993c02da4e25b801c1092b56044
Anya Wynne <avkwynne@gmail.com>
2021-03-23 11:21:51 -0500

    content with veiew list of collectibles page for now

--- frontEnd/src/app/view-list-of-collectibles/view-list-of-collectibles.component.ts
+++ frontEnd/src/app/view-list-of-collectibles/view-list-of-collectibles.component.ts
@@ -7,15 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewListOfCollectiblesComponent implements OnInit {
 
-  items: string[] = ['https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
-  'https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394',
-  'https://media.istockphoto.com/photos/balls-picture-id488816326',
-  'https://media.istockphoto.com/photos/rocket-on-black-background-picture-id172288174',
-  'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
-  'https://media.istockphoto.com/photos/antique-change-picture-id95520796',
-  'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
-  'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
-  'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg'];
+  items = [{
+    imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+  {
+    imagePath: 'https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/balls-picture-id488816326',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/rocket-on-black-background-picture-id172288174',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/antique-change-picture-id95520796',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/antique-change-picture-id95520796',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+  {
+    imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
+    title: 'Item Title goes here-very long title',
+    description: 'A few words of description will go here, sample of a few lines of words'
+  },
+
+];
 
   constructor() { }
 
