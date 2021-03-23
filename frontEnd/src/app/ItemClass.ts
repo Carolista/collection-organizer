8e17bcc66ca111eb60e23c999b3ef507e198d809
Anya Wynne <avkwynne@gmail.com>
2021-03-23 16:22:52 -0500

    updated ItemClass - but not a final version

--- frontEnd/src/app/ItemClass.ts
+++ frontEnd/src/app/ItemClass.ts
@@ -1,11 +1,14 @@
 export class Item {
     public id: number;
+    public imagePath: string;
     public title: string;
+    public description: string;
 
-    constructor(title: string )
+    constructor(imagePath: string, title: string, description: string)
         
         {
-        // this.id = id;
+        this.imagePath = imagePath;
         this.title = title; 
+        this.description = description;
     }
 }
