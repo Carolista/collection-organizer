125aa9282da9fe3ad8cc8e7e4897d943ea474f8f
Casey <casey.gardonio@foat.org>
2021-03-24 17:57:44 -0500

    began adding child routes for member-page; need to fix <router-outlet> so components display properly

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { MemberPageComponent } from "./member-page/member-page.component";
 import { AddItemFormComponent } from "./add-item-form/add-item-form.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ViewListOfCollectiblesComponent } from './view-list-of-collectibles/view-list-of-collectibles.component';
+import { ItemDetailComponent } from './view-list-of-collectibles/item-detail/item-detail.component'
 
 const appRoutes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'member-page', component: MemberPageComponent},
-  {path: 'add-item', component: AddItemFormComponent},
+  {path: 'member-page', component: MemberPageComponent, children: [
+    {path: 'my-collections', component: ViewListOfCollectiblesComponent, children: [
+      {path: 'item-detail', component: ItemDetailComponent},
+    ]},
+    {path: 'add-item', component: AddItemFormComponent}
+  ]},
+    //put child routes to collections and item detail pages here
   {path: 'page-not-found', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/page-not-found'}
 ]
