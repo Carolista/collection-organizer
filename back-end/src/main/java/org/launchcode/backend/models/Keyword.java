b0c2a696ea681d420de1df38c2974a8392fa0849
tarabyte84 <tekotthoff@me.com>
2021-03-25 10:17:23 -0500

    Fields, constructors, getters/setters for Keyword class

--- back-end/src/main/java/org/launchcode/backend/models/Keyword.java
+++ back-end/src/main/java/org/launchcode/backend/models/Keyword.java
@@ -1,4 +1,34 @@
 package org.launchcode.backend.models;
 
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Keyword {
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @ManyToMany(mappedBy="keywords")
+    private List<Item> items = new ArrayList<>();
+
+    public Keyword(@Size(max = 500, message = "Description too long!") String description, List<Item> items) {
+        this.description = description;
+    }
+
+    public Keyword() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
 }
