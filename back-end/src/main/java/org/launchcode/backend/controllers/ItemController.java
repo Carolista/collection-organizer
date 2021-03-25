71714517175e7f3ecc6159a462ec62ccca9e7469
Chan <chandler.eubank@gmail.com>
2021-03-24 20:32:40 -0500

    Added updateItem method which still needs tweaking, requires exception to be thrown

--- back-end/src/main/java/org/launchcode/backend/controllers/ItemController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/ItemController.java
@@ -4,8 +4,11 @@ import org.launchcode.backend.models.Item;
 import org.launchcode.backend.models.data.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 
@@ -32,14 +35,25 @@ public class ItemController {
     public Optional<Item> get(@PathVariable("id") long id) {
         return itemRepository.findById(id);
     }
+
+    @PutMapping("/item/{id}") //Still editing
+    public ResponseEntity<Item> updateItem(@PathVariable(value = "id") long id,
+                                           @Valid @RequestBody Item employeeDetails, Errors errors) {
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new ADD-EXCEPTION-HERE("Item not found for this id :: " + id));
+
+        item.setTitle(item.getTitle());
+        final Item updatedItem = itemRepository.save(item);
+        return ResponseEntity.ok(updatedItem);
+    }
+
+
 }
 
 
-    //TODO: processAddItemForm
+    //TODO: processAddItemForm??
 
     //TODO: displayViewItem
 
     //TODO: deleteItem
-
-    //TODO: take a look at FE form creation page to verify keywords needed to properly render view - i.e. "title?"
 
