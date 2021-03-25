2cc5bc61e6963950c9c71fab0aae070b6aa4878c
tarabyte84 <tekotthoff@me.com>
2021-03-25 10:51:55 -0500

    Added category/item relationship in category class, created getter

--- back-end/src/main/java/org/launchcode/backend/models/Category.java
+++ back-end/src/main/java/org/launchcode/backend/models/Category.java
@@ -1,6 +1,15 @@
 package org.launchcode.backend.models;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category extends AbstractEntity{
+
+    @OneToMany
+    @JoinColumn(name="category")
+    private List<Item> items = new ArrayList<>();
 
     public Category(String title) {
         super(title);
@@ -8,4 +17,9 @@ public class Category extends AbstractEntity{
 
     public Category() {
     }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
 }
