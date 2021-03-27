41bfe028c1152a03362745855c5edec29419d159
tarabyte84 <tekotthoff@me.com>
2021-03-27 15:40:06 -0500

    Problems with expanded-item-class, deleted and created expand-item-class-v2

--- back-end/src/main/java/org/launchcode/backend/models/Item.java
+++ back-end/src/main/java/org/launchcode/backend/models/Item.java
@@ -1,8 +1,31 @@
 package org.launchcode.backend.models;
 import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Item extends AbstractEntity {
+
+    private String imagePath;
+    private String creator;
+
+//    @NotNull(message="Category required")
+//    @ManyToOne
+//    private Category category;
+//
+//    @ManyToMany
+//    private List<Keyword> keywords = new ArrayList<>();
+
+
+    private int yearCreated;
+    private String placeOfOrigin;
+    private int yearAcquired;
+    private String condition;
+    private String mediaType;
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    private String references;
 
     public Item(String title) {
         super(title);
