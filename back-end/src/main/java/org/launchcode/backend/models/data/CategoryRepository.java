4206843e48d188569a473fc145be983ae9d835a5
tarabyte84 <tekotthoff@me.com>
2021-03-27 16:06:51 -0500

    extended CategoryRepository from Crud

--- back-end/src/main/java/org/launchcode/backend/models/data/CategoryRepository.java
+++ back-end/src/main/java/org/launchcode/backend/models/data/CategoryRepository.java
@@ -1,4 +1,7 @@
 package org.launchcode.backend.models.data;
 
-public interface CategoryRepository {
+import org.launchcode.backend.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
