0654f17403219007865c1929c0c241249633c653
Anya Wynne <avkwynne@gmail.com>
2021-03-28 16:58:47 -0500

    created a function to delete an item in the browser only

--- frontEnd/src/app/item-detail/item-detail.component.ts
+++ frontEnd/src/app/item-detail/item-detail.component.ts
@@ -16,12 +16,6 @@ export class ItemDetailComponent implements OnInit {
   id: number;
   new: string;
 
-  // itemData = {
-  //   imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
-  //   title: 'Item Title goes here-very long title',
-  //   description: 'A few words of description will go here, sample of a few lines of words.  The details of the item will be expended a back-end team will add more controllers and we have one common class to work with.'
-  // };
-
   constructor(private viewItemsService: ViewItemsService,
               private route: ActivatedRoute,
               private router: Router
@@ -40,6 +34,16 @@ export class ItemDetailComponent implements OnInit {
 
     this.itemData = this.viewItemsService.getItemData(this.id);
 
+  }
+
+  onDelete(){
+    let confirm = window.confirm(
+      'This item will be permanently deleted. Are you sure you want to delete this itme?');
+    if (confirm === true) {
+    this.viewItemsService.deleteItem(this.id);
+    //add code to upload the changes to the back end
+    this.router.navigate(['/member-page']);
+    } 
   }
 
 }
