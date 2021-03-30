01bd31c87b3b960ba642f0973bb0fd8bca90f0a3
Anya Wynne <avkwynne@gmail.com>
2021-03-30 14:49:09 -0500

    form updated and working

--- frontEnd/src/app/add-item-form/add-item-form.component.ts
+++ frontEnd/src/app/add-item-form/add-item-form.component.ts
@@ -24,33 +24,33 @@ export class AddItemFormComponent implements OnInit {
 
   addItemForm: FormGroup;
   formSubmitted = false;
-  browseMainCategories = ['Fine Arts', 'Culture', 'Decorative arts', 'Machines and Transportation',
-    'Fashion and Textiles', 'Natural History'];
+  browseMainCategories = [' Fine Arts', ' Culture', ' Decorative arts', ' Machines and Transportation',
+    ' Fashion and Textiles', ' Natural History'];
 
-  fineArts = ['Painting', 'Sculpture', 'Prints/Photographs/Drawings/Digital', 'European',
-  ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', 'Near and Middle Eastern', 
-  'American', 'Pre-20th century', 'Modern/Contemporary'];
+  fineArts = [' Painting', ' Sculpture', ' Prints/Photographs/Drawings/Digital', ' European',
+  ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', ' Near and Middle Eastern', 
+  ' American', ' Pre-20th century', ' Modern/Contemporary'];
 
-  culture = ['Collectibles (Figurines/toys/misc.)', 'Ephemera (Autographs/Advertising/Posters/etc.)', 
-  'Numismatics/Coins and medals/Monies ', 'Military and wartime', 'Philately/Stamps',
-  'Sports', 'Political/Fraternal/Organizational', 'Breweriana/Tobacciana/Petroliana',
-  'Entertainment media (music/movies/video games)', 'Print entertainment media (Comics/Books/Newspapers)'];
+  culture = [' Collectibles (Figurines/toys/misc.)', ' Ephemera (Autographs/Advertising/Posters/etc.)', 
+  ' Numismatics/Coins and medals/Monies ', ' Military and wartime', ' Philately/Stamps',
+  ' Sports', ' Political/Fraternal/Organizational', ' Breweriana/Tobacciana/Petroliana',
+  ' Entertainment media (music/movies/video games)', ' Print entertainment media (Comics/Books/Newspapers)'];
 
-  decorativeArts= ['Pre-20th century', 'Victorian Era', 'Art Deco/Art Nouveau/Arts and Crafts',
-  'Mid-Century Modern', 'Ceramics/Pottery/China/Porcelain', 'Folk Art',
-  'Textiles','Furniture','Architecture'];
+  decorativeArts= [' Pre-20th century', ' Victorian Era', ' Art Deco/Art Nouveau/Arts and Crafts',
+  ' Mid-Century Modern', ' Ceramics/Pottery/China/Porcelain', ' Folk Art',
+  ' Textiles',' Furniture',' Architecture'];
 
-  machinesAndTransportation = ['Cameras', 'Cars and Motorcycles', 'Aviation and Space',
-  'Nautical', 'Electronics', 'Models	(cars, trains, etc.)',
-  'Radios', 'Telephones', 'Office', 'Clocks'
+  machinesAndTransportation = [' Cameras', ' Cars and Motorcycles', ' Aviation and Space',
+  ' Nautical', ' Electronics', ' Models	(cars, trains, etc.)',
+  ' Radios', ' Telephones', ' Office', ' Clocks'
 ];
 
-fashionAndTextiles = ['Clothing and shoes', 'Fine Jewelry', 'Costume Jewelry', 
-'Accessories (watches, handbags, pens, etc.)', 'Arms and Armor (incl. knives/swords/firearms/etc.)'];
+fashionAndTextiles = [' Clothing and shoes', ' Fine Jewelry', ' Costume Jewelry', 
+' Accessories (watches, handbags, pens, etc.)', ' Arms and Armor (incl. knives/swords/firearms/etc.)'];
 
-naturalHistory = ['Animals/Zoology', 'Botany', 'Shells',
-'Fossils', 'Rocks, minerals, and gems', 'Precious metals',
-'Natural history collateral (books/guides/tools/etc.)', 'Medical/Scientific', 'Maps/Globes' ];
+naturalHistory = [' Animals/Zoology', ' Botany', ' Shells',
+' Fossils', ' Rocks, minerals, and gems', ' Precious metals',
+' Natural history collateral (books/guides/tools/etc.)', ' Medical/Scientific', ' Maps/Globes' ];
 
   constructor(private http: HttpClient, private router: Router) { }
 
