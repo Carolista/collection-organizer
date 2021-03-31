fa1a44148c6df64647113ced7fc5ef3cff25f450
Anya Wynne <avkwynne@gmail.com>
2021-03-31 18:32:05 -0500

    have a working post and fetch methods

--- frontEnd/src/app/view-list-of-collectibles/view-list-of-collectibles.component.ts
+++ frontEnd/src/app/view-list-of-collectibles/view-list-of-collectibles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewItemsService } from '../viewItems.service';
+import { Item } from '../ItemClass'
 
 @Component({
   selector: 'app-view-list-of-collectibles',
@@ -9,14 +10,17 @@ import { ViewItemsService } from '../viewItems.service';
 export class ViewListOfCollectiblesComponent implements OnInit {
 
   viewMyCollection: boolean = true;
-  items = [];
+  items: Item[] = [];
 
   constructor(private viewItemsService: ViewItemsService) { }
 
   ngOnInit(): void {
-    this.items = this.viewItemsService.getItems();
-
+    // this.items = this.viewItemsService.getItems();
     this.viewItemsService.fetchItems();
+
+    this.items = this.viewItemsService.fetchedItems;
+    // this.items = this.viewItemsService.getFetchedItems();
+    console.log(this.items);
 
     /*alternative way of getting items without using a function, which might be better for once we are fetching to an array in that service:*/
     // this.items = this.viewItemsService.listOfItems;
--- frontEnd/src/app/viewItems.service.ts
+++ frontEnd/src/app/viewItems.service.ts
@@ -2,13 +2,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 // import { Subject } from "rxjs";
-import { Item } from "./ItemClassTemp"
+import { Item } from "./ItemClass"
 
 @Injectable({providedIn:'root'})
 export class ViewItemsService {
 
   //soon list of items will be fetching an array of objects from the back end.
-  listOfItems: Item[] = [
+  listOfItems: any = [
     {
       imagePath: 'https://secure.img1-ag.wfcdn.com/im/18951009/resize-h800%5Ecompr-r85/4007/4007560/Sovereign+of+The+Seas+Monumental+Model+Ship.jpg',
       title: 'Item Title goes here-very long title',
@@ -66,7 +66,7 @@ export class ViewItemsService {
     }
   ];
 
-  fetchedItems: any = [];  // change to Item[] after I updated Item class to mirror the back end set up.
+  fetchedItems: Item[] = [];  // change to Item[] after I updated Item class to mirror the back end set up.
 
   constructor(private http: HttpClient){};
   //WHEN FETCH FUNCTION IS WORKING REPLACE THE ARRAY NAME TO FETCHEDITEMS
@@ -82,15 +82,24 @@ export class ViewItemsService {
       console.log('fetchItems() method is called from the view-list-of-collectibles component');
       console.log (this.fetchedItems);
     }
-
-  getItems(){
+    getFetchedItems(){
+      console.log(this.fetchedItems);
+      console.log('this fetched items talks to view -list component')
+      return this.fetchedItems.slice(0); 
+    }
+ 
+ 
+    getItems(){
     console.log("got items");
     return this.listOfItems.slice();  
   }
 
+  // getItemData(index: number){
+  //     return this.listOfItems[index];
+  // }
   getItemData(index: number){
-      return this.listOfItems[index];
-  }
+    return this.fetchedItems[index];
+}
 
   deleteItem(index: number){
     this.listOfItems.splice(index, 1);
