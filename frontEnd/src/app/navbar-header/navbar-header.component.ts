ba586ecf3f6d36e32b5383e62faae55f23a0c2e5
Anya Wynne <avkwynne@gmail.com>
2021-04-06 14:29:21 -0500

    ready to test search method

--- frontEnd/src/app/navbar-header/navbar-header.component.ts
+++ frontEnd/src/app/navbar-header/navbar-header.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ViewItemsService } from '../viewItems.service'
+import { FormControl, FormGroup } from '@angular/forms';
+import { ViewItemsService } from '../viewItems.service';
+
 
 @Component({
   selector: 'app-navbar-header',
@@ -8,10 +11,32 @@ import { ViewItemsService } from '../viewItems.service'
 })
 export class NavbarHeaderComponent implements OnInit {
 
-  constructor(private viewItemsService: ViewItemsService) { }
+  userSearch: FormGroup;
+
+  constructor(private viewItemsService: ViewItemsService,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.userSearch = new FormGroup ({
+      'userInput': new FormControl(null)
+    });
+
   }
+
+  onSearch(){
+    console.log(typeof this.userSearch.get('userInput').value);
+    
+    //onces this method is proven to be working, I will move it into the service
+    //for testing purposes only, I leave it in this component.
+
+      const searchStringParams = { params: new HttpParams({fromString: this.userSearch.get('userInput').value}) };
+      this.http.get("http://localhost:8080/api/item", searchStringParams).subscribe(response =>{
+        console.log(response);
+      });
+    
+  }
+
   // decided to move this to view-list-of-collections ngOnInit
   // // onFetchMyCollectionData(){
   // //   this.viewItemsService.fetchItems();
