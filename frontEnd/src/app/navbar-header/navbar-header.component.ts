f27e421d1dc007c0b842e767606c65649236b329
Anya Wynne <avkwynne@gmail.com>
2021-04-07 12:44:41 -0500

    more progress on navbar-needs more work

--- frontEnd/src/app/navbar-header/navbar-header.component.ts
+++ frontEnd/src/app/navbar-header/navbar-header.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router, Params } from '@angular/router';
 import { CategoriesService } from '../categories.service';
 import { ViewItemsService } from '../viewItems.service';
 
@@ -15,53 +14,31 @@ export class NavbarHeaderComponent implements OnInit {
 
   categories:string[];
   userSearch: FormGroup;
-  subCategory = ' Fine Arts';
+  subCategory:string;
   categoryArrIndex: number;
+  
+  subCategoriesArr = [
+    [' Painting', ' Sculpture', ' Prints/Photographs/Drawings/Digital', ' European',
+  ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', ' Near and Middle Eastern', 
+  ' American', ' Pre-20th century', ' Modern/Contemporary']
+  ]
 
+  selectedSubcategoryArr: string [];
+  
   fineArts = [' Painting', ' Sculpture', ' Prints/Photographs/Drawings/Digital', ' European',
   ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', ' Near and Middle Eastern', 
   ' American', ' Pre-20th century', ' Modern/Contemporary'];
 
   constructor(private viewItemsService: ViewItemsService,
               private http: HttpClient,
-              private categoriesService: CategoriesService,
-              private router: Router,
-              private route: ActivatedRoute) { }
+              private categoriesService: CategoriesService
+              ) { }
 
   ngOnInit(): void {
     this.categories = this.categoriesService.browseMainCategories;
     this.userSearch = new FormGroup ({
       'userInput': new FormControl(null)
     });
-
-    // this.route.params.subscribe(
-    //   (params: Params) => {
-    //     this.categoryArrIndex = +params['id'];
-    //     // this.itemData = this.viewItemsService.getItemData(this.id);
-    //     // this.viewItemsService.fetchedItemsIndex = this.id;
-    //     console.log( this.categoryArrIndex);
-    //   }
-    // this.route.params.subscribe(
-    //   (params: Params) => {
-    //     // this.categoryArrIndex = +params['chosenCategory'];
-    //     this.categoryArrIndex = 0;
-    //     console.log( this.categoryArrIndex);
-    //     this.subCategory = this.categories[this.categoryArrIndex];
-    //     // console.log(this.subCategory);
-    //   }
-    // )
-
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.categoryArrIndex = +params['chosenCategory'];
-        // this.itemData = this.viewItemsService.getItemData(this.id);
-        // this.viewItemsService.fetchedItemsIndex = this.id;
-        console.log( this.categoryArrIndex);
-      }
-
-    );
-
-
 
   }
 
@@ -78,9 +55,11 @@ export class NavbarHeaderComponent implements OnInit {
     
   }
 
-  onBrowse(){
+  onBrowse(clickedArrIndex:number){
+    this.categoryArrIndex=clickedArrIndex;
+    this.subCategory = this.categories[this.categoryArrIndex];
+    this.selectedSubcategoryArr = this.subCategoriesArr[clickedArrIndex];
     
-    console.log(this.subCategory);
   }
 
   // decided to move this to view-list-of-collections ngOnInit
