27a5b439c79ff550e98b24a44efc1ce45aea232b
Anna McBride <mcbrideanna28@gmail.com>
2021-04-07 10:16:54 -0500

    ItemRepository with search method added

---
+++ back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
@@ -0,0 +1,12 @@
+package org.launchcode.backend.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SearchController {
+
+
+
+
+}
--- back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
+++ back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
@@ -2,14 +2,24 @@ package org.launchcode.backend.models.data;
 
 
 import org.launchcode.backend.models.Item;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 //@Transactional
 public interface ItemRepository extends CrudRepository <Item, Long>{
 
     void deleteItemById(Long id);
+
+
+    @Query(value = "SELECT * FROM item WHERE "
+            + "MATCH(title, category, cond, creator, description, place_of_origin, refs, sub_category) "
+            + "AGAINST(?1)",
+            nativeQuery = true)
+    public List<Item> search(String keyword);
+
 }
