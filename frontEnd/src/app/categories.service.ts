87aae4f1f27e957864d846ed685b47af33c52094
Anya Wynne <avkwynne@gmail.com>
2021-04-08 09:54:49 -0500

    updated subcotegories in nab bar

--- frontEnd/src/app/categories.service.ts
+++ frontEnd/src/app/categories.service.ts
@@ -6,23 +6,23 @@ export class CategoriesService {
     browseMainCategories = ['Fine Arts', 'Culture', 'Decorative arts', 'Machines & Transport',
     'Fashion and Textiles', 'Natural History'];
     subCategoriesArr=[
-        [' Painting', ' Sculpture', ' Prints/Photographs/Drawings/Digital', ' European',
-        ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', ' Near and Middle Eastern', 
+        [' Painting', ' Sculpture', ' Photographs, Prints & Digital Art', ' European',
+        ' African, Native American, Aboriginal & Asian Art', ' Middle Eastern', 
         ' American', ' Pre-20th century', ' Modern/Contemporary'],
-        [' Collectibles (Figurines/toys/misc.)', ' Ephemera (Autographs/Advertising/Posters/etc.)', 
-        ' Numismatics/Coins and medals/Monies ', ' Military and wartime', ' Philately/Stamps',
-        ' Sports', ' Political/Fraternal/Organizational', ' Breweriana/Tobacciana/Petroliana',
-        ' Entertainment media (music/movies/video games)', ' Print entertainment media (Comics/Books/Newspapers)'],
-        [' Pre-20th century', ' Victorian Era', ' Art Deco/Art Nouveau/Arts and Crafts',
-        ' Mid-Century Modern', ' Ceramics/Pottery/China/Porcelain', ' Folk Art',
+        [' Collectibles', ' Ephemera', 
+        ' Numismatics', ' Military and wartime', ' Philately',
+        ' Sports', ' Political', ' Breweriana/Tobacciana/Petroliana',
+        ' Entertainment media', ' Print entertainment media'],
+        [' Pre-20th century', ' Victorian Era', ' Art Nouveau/Arts and Crafts',
+        ' Mid-Century Modern', ' Ceramics', ' Folk Art',
         ' Textiles',' Furniture',' Architecture'],
         [' Cameras', ' Cars and Motorcycles', ' Aviation and Space',
-        ' Nautical', ' Electronics', ' Models	(cars, trains, etc.)',
+        ' Nautical', ' Electronics', ' Models',
         ' Radios', ' Telephones', ' Office', ' Clocks'],
         [' Clothing and shoes', ' Fine Jewelry', ' Costume Jewelry', 
-        ' Accessories (watches, handbags, pens, etc.)', ' Arms and Armor (incl. knives/swords/firearms/etc.)'],
-        [' Animals/Zoology', ' Botany', ' Shells',
-        ' Fossils', ' Rocks, minerals, and gems', ' Precious metals',
-        ' Natural history collateral (books/guides/tools/etc.)', ' Medical/Scientific', ' Maps/Globes' ]
+        ' Accessories', ' Arms and Armor'],
+        [' Zoology', ' Botany', ' Shells',
+        ' Fossils', ' Minerals & Gems', ' Precious Metals',
+        ' Natural history collateral', ' Medical & Scientific', ' Maps & Globes' ]
       ];
 }
