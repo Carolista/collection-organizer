88fd799d2c952976b67bb1ebf6dd47497bf8990c
Anna McBride <mcbrideanna28@gmail.com>
2021-04-09 18:18:24 -0500

    @CrossOrigin added

--- back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ import java.util.List;
 
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/search")
 public class SearchController {
 
