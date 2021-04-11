31045f0c225d490ce300c255d56a1e85d813ef67
Anya Wynne <avkwynne@gmail.com>
2021-04-11 11:30:43 -0500

    updated categories view

--- frontEnd/src/app/add-item-form/add-item-form.component.ts
+++ frontEnd/src/app/add-item-form/add-item-form.component.ts
@@ -20,29 +20,17 @@ export class AddItemFormComponent implements OnInit, OnDestroy {
   // formSubmitted = false;
   browseMainCategories: string[];
 
-  fineArts = [' Painting', ' Sculpture', ' Prints/Photographs/Drawings/Digital', ' European',
-  ' Africa/Oceania/Pre-Columbian Americas/Native American/Aboriginal Asian', ' Near and Middle Eastern', 
-  ' American', ' Pre-20th century', ' Modern/Contemporary'];
+  fineArts = this.categoriesService.subCategoriesArr[0];
 
-  culture = [' Collectibles (Figurines/toys/misc.)', ' Ephemera (Autographs/Advertising/Posters/etc.)', 
-  ' Numismatics/Coins and medals/Monies ', ' Military and wartime', ' Philately/Stamps',
-  ' Sports', ' Political/Fraternal/Organizational', ' Breweriana/Tobacciana/Petroliana',
-  ' Entertainment media (music/movies/video games)', ' Print entertainment media (Comics/Books/Newspapers)'];
+  culture = this.categoriesService.subCategoriesArr[1];
 
-  decorativeArts= [' Pre-20th century', ' Victorian Era', ' Art Deco/Art Nouveau/Arts and Crafts',
-  ' Mid-Century Modern', ' Ceramics/Pottery/China/Porcelain', ' Folk Art',
-  ' Textiles',' Furniture',' Architecture'];
+  decorativeArts= this.categoriesService.subCategoriesArr[2];
 
-  machinesAndTransportation = [' Cameras', ' Cars and Motorcycles', ' Aviation and Space',
-  ' Nautical', ' Electronics', ' Models	(cars, trains, etc.)',
-  ' Radios', ' Telephones', ' Office', ' Clocks'];
+  machinesAndTransportation = this.categoriesService.subCategoriesArr[3];
 
-fashionAndTextiles = [' Clothing and shoes', ' Fine Jewelry', ' Costume Jewelry', 
-' Accessories (watches, handbags, pens, etc.)', ' Arms and Armor (incl. knives/swords/firearms/etc.)'];
+  fashionAndTextiles = this.categoriesService.subCategoriesArr[4];
 
-naturalHistory = [' Animals/Zoology', ' Botany', ' Shells',
-' Fossils', ' Rocks, minerals, and gems', ' Precious metals',
-' Natural history collateral (books/guides/tools/etc.)', ' Medical/Scientific', ' Maps/Globes' ];
+  naturalHistory = this.categoriesService.subCategoriesArr[5];
 
   constructor(private http: HttpClient, 
               private router: Router,
