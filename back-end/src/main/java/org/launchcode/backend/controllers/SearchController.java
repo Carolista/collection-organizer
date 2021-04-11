59b8964cd87dbb9c34e28ca59ef041c3e6ba9794
Anna McBride <mcbrideanna28@gmail.com>
2021-04-11 18:11:58 -0500

    working with new search ideas

--- back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
@@ -5,6 +5,8 @@ import org.launchcode.backend.models.Item;
 import org.launchcode.backend.models.data.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.List;
 @Controller
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "api/search", method = RequestMethod.POST)
+//@RequestMapping("search")
 public class SearchController {
 
     @Autowired
@@ -27,8 +30,47 @@ public class SearchController {
 
     }
 
+    //================start search option 2=================
 
 
+//    @GetMapping("")
+//    public List<Item> displaySearchResults(@Param("searchTerm")String searchTerm, @Param("category")String category,
+//                                           @Param("subCategory")String subCategory) {
+//
+//
+//     List<Item> searchResult = new ArrayList<>();
+//        if (searchTerm == null && category == null && subCategory == null) {
+//            searchResult = (List<Item>) itemRepository.findAll();
+//        }
+//        if (searchTerm != null && category == null && subCategory == null) {
+//            searchResult = itemRepository.findBySearchTerm(searchTerm);
+//        }
+//        if (searchTerm == null && category != null && subCategory == null) {
+//            searchResult = itemRepository.findByCategory(category);
+//        }
+//        if (searchTerm == null && category == null && subCategory != null) {
+//            searchResult = itemRepository.findBySubCategory(subCategory);
+//        }
+//        if (searchTerm != null && category != null && subCategory == null) {
+//            searchResult = itemRepository.findBySearchTermAndCategory(searchTerm, category);
+//        }
+//        if (searchTerm == null && category != null && subCategory != null) {
+//            searchResult = itemRepository.findByCategoryAndSubCategory(category, subCategory);
+//        }
+//        if (searchTerm != null && category != null && subCategory != null) {
+//            searchResult = itemRepository.findBySearchTermAndCategoryAndSubCategory(searchTerm, category, subCategory);
+//        }
+//        return searchResult;
+//
+//
+//    }
+
+//}
+    ////=============end of search option 2 ==========================
+
+
+
+//*********start search option 1****************************
     @GetMapping
     public String searchResult(@Param(value = "search") String search) {
         ArrayList<Item> searchResult = new ArrayList<Item>();
@@ -38,11 +80,15 @@ public class SearchController {
             }
         }
 
-        return "item/search"; // I've tried "search" "item/search" "searchResult"
+        return "searchResult"; // I've tried "search" "item/search" "searchResult" and more
     }
-
-}
 //
+}
+//////*****End search option 1************************
+
+//
+
+////rejected code I am not ready to delete yet
 //@GetMapping
 ////@GetMapping
 //    public String search (@Param("searchTerm") String searchTerm) {
--- back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
+++ back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
@@ -4,6 +4,7 @@ package org.launchcode.backend.models.data;
 import org.launchcode.backend.models.Item;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,21 +15,51 @@ import java.util.List;
 
 @Repository
 //@Transactional
-public interface ItemRepository extends CrudRepository <Item, Long>{
+public interface ItemRepository extends CrudRepository <Item, Long> {
 
     void deleteItemById(Long id);
 
 
+    // =======================goes with search option 2==================
+//    List<Item> findBySearchTerm(String searchTerm);
+//
+//    List<Item> findByCategory(String category);
+//
+//    List<Item> findBySubCategory(String subCategory);
+//
+//    List<Item> findBySearchTermAndCategory(String searchTerm, String category);
+//
+//    List<Item> findByCategoryAndSubCategory(String category, String subCategory);
+//
+//    List<Item> findBySearchTermAndCategoryAndSubCategory(String searchTerm, String category, String subCategory);
+//
+//    @Query(value = "SELECT * FROM item WHERE "
+//            + "MATCH(title, category, cond, creator, description, place_of_origin, refs, sub_category) "
+//            + "AGAINST(?1)",
+//            nativeQuery = true)
+//    public List<Item> search(@Param("searchTerm") String searchTerm, @Param("category") String category,
+//                             @Param("subCategory") String subCategory);
+
+
+    //==============end search option 2=============
 
 
 
 
+    //***********start search option 1*******************
     @Query(value = "SELECT * FROM item WHERE "
             + "MATCH(title, category, cond, creator, description, place_of_origin, refs, sub_category) "
             + "AGAINST(?1)",
             nativeQuery = true)
 
-    public ArrayList<Item> search(String searchResult);
+   public ArrayList<Item> search(String searchResult);
+
+//********************end search option 1*******************
+
+
+
+
+
 
 
 
@@ -39,7 +70,7 @@ public interface ItemRepository extends CrudRepository <Item, Long>{
 //    }
 
 
-
-
 }
+
+
 
