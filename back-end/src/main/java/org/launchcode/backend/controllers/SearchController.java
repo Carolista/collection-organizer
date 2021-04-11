fae26bac05cd01d3dd8a1ea6e33c64fbd505351c
tarabyte84 <tekotthoff@me.com>
2021-04-11 15:51:50 -0500

    Playing with searchController to see if I could get it to work.

--- back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/SearchController.java
@@ -6,41 +6,76 @@ import org.launchcode.backend.models.data.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Controller
+@RequestMapping("/api/search")
 public class SearchController {
 
     @Autowired
     public ItemRepository itemRepository;
-    private Object Item;
 
-    public SearchController(){
+
+
+    public List<Item> searchResult = new ArrayList<>();
+
+
+    @GetMapping("")
+    public List<Item> displaySearchResults(@Param("searchTerm")String searchTerm, @Param("category")String category,
+                             @Param("subCategory")String subCategory) {
+        if(searchTerm == null && category == null && subCategory == null) {
+            searchResult = (List<Item>) itemRepository.findAll();
+        }
+        if(searchTerm != null && category == null && subCategory == null) {
+            searchResult = itemRepository.findBySearchTerm(searchTerm);
+        }
+        if(searchTerm == null && category != null && subCategory == null) {
+            searchResult = itemRepository.findByCategory(category);
+        }
+        if(searchTerm == null && category == null && subCategory != null) {
+            searchResult = itemRepository.findBySubCategory(subCategory);
+        }
+        if(searchTerm != null && category != null && subCategory == null) {
+            searchResult = itemRepository.findBySearchTermAndCategory(searchTerm, category);
+        }
+        if(searchTerm == null && category != null && subCategory != null) {
+            searchResult = itemRepository.findByCategoryAndSubCategory(category, subCategory);
+        }
+        if(searchTerm != null && category != null && subCategory != null) {
+            searchResult = itemRepository.findBySearchTermAndCategoryAndSubCategory(searchTerm, category, subCategory);
+        }
+
+        return searchResult;
+
 
     }
+
+//    public SearchController(){
+//
+//    }
 
 
 //Anya has hers set to /api/item, should mine be at this location too?
 
-@GetMapping("/search")
-    public String search (@Param("keyWord") String keyWord) {   //@Param set to value instead of keyword?
-
-        List<Item> searchResult = itemRepository.search(keyWord);
-        if (searchResult.equals("all") || searchResult.equals("")) {
-            Item = itemRepository.findAll();
-        } else if (searchResult.equals("keyWord")) {
-
-            Item = itemRepository.search(keyWord);
-        }
-           return "searchResult";
-        }
+//@GetMapping("/search")
+//    public String search (@Param("keyWord") String keyWord) {   //@Param set to value instead of keyword?
+//
+//        List<Item> searchResult = itemRepository.search(keyWord);
+//        if (searchResult.equals("all") || searchResult.equals("")) {
+//            Item = itemRepository.findAll();
+//        } else if (searchResult.equals("keyWord")) {
+//
+//            Item = itemRepository.search(keyWord);
+//        }
+//           return "searchResult";
+//        }
 
 
 
--- back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
+++ back-end/src/main/java/org/launchcode/backend/models/data/ItemRepository.java
@@ -2,8 +2,10 @@ package org.launchcode.backend.models.data;
 
 
 import org.launchcode.backend.models.Item;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +20,39 @@ public interface ItemRepository extends CrudRepository <Item, Long>{
 
     void deleteItemById(Long id);
 
+//    public List<Item> search(String keyWord);
 
+    List<Item> findBySearchTerm(String searchTerm);
 
+    List<Item> findByCategory(String category);
 
+    List<Item> findBySubCategory(String subCategory);
+
+    List<Item> findBySearchTermAndCategory(String searchTerm, String category);
+
+    List<Item> findByCategoryAndSubCategory(String category, String subCategory);
+
+    List<Item> findBySearchTermAndCategoryAndSubCategory(String searchTerm, String category, String subCategory);
+
+//    @Query("select e from Employee e where e.deptId = :deptId")
+//    List<Employee> findEmployeeByDeptId(@Param("deptId") Long departmentId);
 
 
     @Query(value = "SELECT * FROM item WHERE "
             + "MATCH(title, category, cond, creator, description, place_of_origin, refs, sub_category) "
             + "AGAINST(?1)",
             nativeQuery = true)
-    public List<Item> search(String keyWord);
+    List<Item> search(@Param("searchTerm") String searchTerm, @Param("category")String category,
+                      @Param("subCategory")String subCategory);
+
+
+
+
+//    @Query(value = "SELECT * FROM item WHERE "
+//            + "MATCH(title, category, cond, creator, description, place_of_origin, refs, sub_category) "
+//            + "AGAINST(?1)",
+//            nativeQuery = true)
+//    public List<Item> search(String keyWord);
 
 //    @PostMapping("/search")
 //    public List<Item> getFilteredData(@RequestBody Item item) {
