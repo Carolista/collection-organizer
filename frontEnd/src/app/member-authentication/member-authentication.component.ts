9a5b8370aa33f1639eb7ddbc30eeb4c08af4ffe8
Casey <casey.gardonio@foat.org>
2021-04-12 16:53:55 -0500

    adjusted formatting on member-authentication html

--- frontEnd/src/app/member-authentication/member-authentication.component.ts
+++ frontEnd/src/app/member-authentication/member-authentication.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-authentication.component.scss']
 })
 export class MemberAuthenticationComponent implements OnInit {
+  isSignUp = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSwitchAuthMode(){
+    this.isSignUp = !this.isSignUp;
   }
 
 }
