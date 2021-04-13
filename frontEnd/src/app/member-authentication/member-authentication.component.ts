ea8e56269b973a0385de82e0c3013197c5974703
Casey <casey.gardonio@foat.org>
2021-04-13 12:11:43 -0500

    added field validation and object to capture user data to member-authentication form; validation error messages need additional work

--- frontEnd/src/app/member-authentication/member-authentication.component.ts
+++ frontEnd/src/app/member-authentication/member-authentication.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
+//currently uses the template-driven approach; return to consider using reactive approach
 @Component({
   selector: 'app-member-authentication',
   templateUrl: './member-authentication.component.html',
@@ -7,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberAuthenticationComponent implements OnInit {
   isSignUp = true;
+  memberData = {
+    email: '',
+    password: ''
+  }
+  // email: string = '';
+  // password: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmitSignIn(form: NgForm) {
+    console.log(form.value);
+    this.memberData.email = form.value.email; 
+    this.memberData.password = form.value.password;
+    console.log(this.memberData);
+    form.reset();
   }
 
   onSwitchAuthMode(){
