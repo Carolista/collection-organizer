1e90b0f37e9e54d9dab0281ba42961cafa85c885
tarabyte84 <tekotthoff@me.com>
2021-04-14 14:11:02 -0500

    Added enum classes for category and subcategory. added values to SubcategoryType.

---
+++ back-end/src/main/java/org/launchcode/backend/models/SubcategoryType.java
@@ -0,0 +1,12 @@
+package org.launchcode.backend.models;
+
+public enum SubcategoryType {
+
+    FINEARTS,
+    CULTURE,
+    DECORATIVEARTS,
+    MACHINESTRANSPORT,
+    FASHIONTEXTILES,
+    NATURALHISTORY
+
+    }
