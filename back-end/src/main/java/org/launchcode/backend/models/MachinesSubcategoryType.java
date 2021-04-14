726ba47a78ea290108670bbd06244944c5fdcdfe
tarabyte84 <tekotthoff@me.com>
2021-04-14 16:00:23 -0500

    Added MachinesSubcategoryType and values, constructor, getter

---
+++ back-end/src/main/java/org/launchcode/backend/models/MachinesSubcategoryType.java
@@ -0,0 +1,26 @@
+package org.launchcode.backend.models;
+
+public enum MachinesSubcategoryType {
+
+    CAMERAS("Cameras"),
+    VEHICLES("Cars and Motorcycles"),
+    AERO("Aviation and Space"),
+    NAUTICAL("Nautical"),
+    ELECTRONICS("Electronics"),
+    MODELS("Models (cars, trains, etc.)"),
+    RADIOS("Radios"),
+    TELEPHONES("Telephones"),
+    OFFICE("Office"),
+    CLOCKS("Clocks");
+
+    private final String displayName;
+
+    MachinesSubcategoryType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    }
