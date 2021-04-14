d6e568e36d80227b6362cda98afdf938c3901d52
tarabyte84 <tekotthoff@me.com>
2021-04-14 14:48:16 -0500

    Corrected mistake, moved categories to correct enum

--- back-end/src/main/java/org/launchcode/backend/models/CategoryType.java
+++ back-end/src/main/java/org/launchcode/backend/models/CategoryType.java
@@ -1,4 +1,12 @@
 package org.launchcode.backend.models;
 
 public enum CategoryType {
+
+    FINEARTS,
+    CULTURE,
+    DECORATIVEARTS,
+    MACHINESTRANSPORT,
+    FASHIONTEXTILES,
+    NATURALHISTORY
+
 }
--- back-end/src/main/java/org/launchcode/backend/models/SubcategoryType.java
+++ back-end/src/main/java/org/launchcode/backend/models/SubcategoryType.java
@@ -2,11 +2,6 @@ package org.launchcode.backend.models;
 
 public enum SubcategoryType {
 
-    FINEARTS,
-    CULTURE,
-    DECORATIVEARTS,
-    MACHINESTRANSPORT,
-    FASHIONTEXTILES,
-    NATURALHISTORY
+
 
     }
