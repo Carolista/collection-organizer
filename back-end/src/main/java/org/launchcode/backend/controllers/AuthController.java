63796b56b4ba8fd63c614d00e3cae3870a9f2f1d
Chan <chandler.eubank@gmail.com>
2021-04-15 15:13:43 -0500

    Created LoginFormDTO package and class

--- back-end/src/main/java/org/launchcode/backend/controllers/AuthController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/AuthController.java
@@ -1,13 +1,38 @@
 package org.launchcode.backend.controllers;
 
+import org.launchcode.backend.models.User;
 import org.launchcode.backend.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 public class AuthController {
 
     @Autowired
     UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Long userId = (Long) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
 
 }
