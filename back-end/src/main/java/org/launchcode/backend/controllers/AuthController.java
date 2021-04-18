60176dd10cf32a04adc71844460a283b097845bf
tarabyte84 <tekotthoff@me.com>
2021-04-18 16:29:18 -0500

    added cors fix to auth controller

--- back-end/src/main/java/org/launchcode/backend/controllers/AuthController.java
+++ back-end/src/main/java/org/launchcode/backend/controllers/AuthController.java
@@ -6,16 +6,14 @@ import org.launchcode.backend.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @Controller
 @RequestMapping("auth") //do we need mapping here? "register"??
 public class AuthController {
