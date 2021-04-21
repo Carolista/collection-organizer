6bd2372865fe911e198d08dce48bdd0895bf69ca
tarabyte84 <tekotthoff@me.com>
2021-04-21 15:40:34 -0500

    grammar/punctuation cleanup

--- frontEnd/src/app/categories.service.ts
+++ frontEnd/src/app/categories.service.ts
@@ -11,19 +11,19 @@ export class CategoriesService {
         ' African, Native American, Aboriginal & Asian Art', ' Middle Eastern', 
         ' American', ' Pre-20th century', ' Modern/Contemporary'],
         [' Collectibles', ' Ephemera', 
-        ' Numismatics', ' Military and wartime', ' Philately',
+        ' Numismatics', ' Military & Wartime', ' Philately',
         ' Sports', ' Political', ' Breweriana/Tobacciana/Petroliana',
-        ' Entertainment media', ' Print entertainment media'],
-        [' Pre-20th century', ' Victorian Era', ' Art Nouveau/Arts and Crafts',
+        ' Entertainment Media', ' Print Entertainment Media'],
+        [' Pre-20th century', ' Victorian Era', ' Art Nouveau/Arts & Crafts',
         ' Mid-Century Modern', ' Ceramics', ' Folk Art',
         ' Textiles',' Furniture',' Architecture'],
-        [' Cameras', ' Cars and Motorcycles', ' Aviation and Space',
+        [' Cameras', ' Cars & Motorcycles', ' Aviation & Space',
         ' Nautical', ' Electronics', ' Models',
         ' Radios', ' Telephones', ' Office', ' Clocks'],
-        [' Clothing and shoes', ' Fine Jewelry', ' Costume Jewelry', 
-        ' Accessories', ' Arms and Armor'],
+        [' Clothing & Shoes', ' Fine Jewelry', ' Costume Jewelry', 
+        ' Accessories', ' Arms & Armor'],
         [' Zoology', ' Botany', ' Shells',
         ' Fossils', ' Minerals & Gems', ' Precious Metals',
-        ' Natural history collateral', ' Medical & Scientific', ' Maps & Globes' ]
+        ' Natural History Collateral', ' Medical & Scientific', ' Maps & Globes' ]
       ];
 }
