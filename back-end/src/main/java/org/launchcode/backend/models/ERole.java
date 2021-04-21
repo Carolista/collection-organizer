8a3d436d88be95417461779516288b5962c48346
tarabyte84 <tekotthoff@me.com>
2021-04-21 11:28:40 -0500

    Added ERole enum and Role class

---
+++ back-end/src/main/java/org/launchcode/backend/models/ERole.java
@@ -0,0 +1,8 @@
+package org.launchcode.backend.models;
+
+public enum ERole {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
---
+++ back-end/src/main/java/org/launchcode/backend/models/Role.java
@@ -0,0 +1,28 @@
+package org.launchcode.backend.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
